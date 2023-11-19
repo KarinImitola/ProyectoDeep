b7370d7a02f66875793c59bfd0e04c589e70ce39
KarinImitola <144546298+KarinImitola@users.noreply.github.com>
2023-11-18 19:28:14 -0500

    Add files via upload

---
+++ real_time_object_detection.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+
+#Get webcam signal
+VideoSignal = cv2.VideoCapture(0)
+# Load YOLO weight files and CFG files
+YOLO_net = cv2.dnn.readNet("real-time-object-detection\yolov3_final.weights","real-time-object-detection\yolov3.cfg")
+
+# YOLO NETWORK reorganization
+classes = []
+with open("real-time-object-detection\custom.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = YOLO_net.getLayerNames()
+output_layers = [layer_names[i- 1] for i in YOLO_net.getUnconnectedOutLayers()]
+
+while True:
+    #Webcam frame
+    ret, frame = VideoSignal.read()
+    h, w, c = frame.shape
+
+    # Enter YOLO
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0),
+    True, crop=False)
+    YOLO_net.setInput(blob)
+    outs = YOLO_net.forward(output_layers)
+
+    class_ids = []
+    confidences = []
+    boxes = []
+
+    for out in outs:
+
+        for detection in out:
+
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+
+            if confidence > 0.5:
+                # Object detected
+                center_x = int(detection[0] * w)
+                center_y = int(detection[1] * h)
+                dw = int(detection[2] * w)
+                dh = int(detection[3] * h)
+                # Rectangle coordinate
+                x = int(center_x - dw / 2)
+                y = int(center_y - dh / 2)
+                boxes.append([x, y, dw, dh])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.45, 0.4)
+
+
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            score = confidences[i]
+
+            # Input bounding box and class information into image
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
+            cv2.putText(frame, label, (x, y - 20), cv2.FONT_ITALIC, 0.5, 
+            (255, 255, 255), 1)
+
+    cv2.imshow("YOLOv3", frame)
+
+    if cv2.waitKey(100) > 0:
+        break
